02b45bb53c9c090535b585f31192b183fd794f71
Hrishikesh Thesserikaran <hrishikeshts@gmail.com>
2020-11-28 12:51:50 +0530

    Add font "Mazzard H"

--- front-end/asd-lab-react-app/src/index.js
+++ front-end/asd-lab-react-app/src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./fonts/MazzardH/MazzardH-Black.woff2";
+import "./fonts/MazzardH/MazzardH-BlackItalic.woff2";
+import "./fonts/MazzardH/MazzardH-Bold.woff2";
+import "./fonts/MazzardH/MazzardH-BoldItalic.woff2";
+import "./fonts/MazzardH/MazzardH-ExtraBold.woff2";
+import "./fonts/MazzardH/MazzardH-ExtraBoldItalic.woff2";
+import "./fonts/MazzardH/MazzardH-ExtraLight.woff2";
+import "./fonts/MazzardH/MazzardH-ExtraLightItalic.woff2";
+import "./fonts/MazzardH/MazzardH-Italic.woff2";
+import "./fonts/MazzardH/MazzardH-Light.woff2";
+import "./fonts/MazzardH/MazzardH-LightItalic.woff2";
+import "./fonts/MazzardH/MazzardH-Medium.woff2";
+import "./fonts/MazzardH/MazzardH-MediumItalic.woff2";
+import "./fonts/MazzardH/MazzardH-Regular.woff2";
+import "./fonts/MazzardH/MazzardH-SemiBold.woff2";
+import "./fonts/MazzardH/MazzardH-SemiBoldItalic.woff2";
+import "./fonts/MazzardH/MazzardH-Thin.woff2";
+import "./fonts/MazzardH/MazzardH-ThinItalic.woff2";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
